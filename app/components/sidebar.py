657e814646bbeb98c9d74aa451eded517a4e5599
E106727_michelin <nicolas.zeisser_ext@michelin.com>
2026-05-28 14:58:04 +0200

    WIP

--- app/components/sidebar.py
+++ app/components/sidebar.py
@@ -12,7 +12,7 @@ def sidebar(state: StateManager):
     with st.sidebar:
         st.title("This is a title")
 
-        st.write(f"User: {st.user.nickname}")
+        st.write(f"Welcome, {st.user.nickname}")
 
         if st.button("Logout"):
             logout(state)
