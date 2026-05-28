cfed0927601c674dc1ac3d234b15f60d49bfb772
E106727_michelin <nicolas.zeisser_ext@michelin.com>
2026-05-28 16:36:32 +0200

    WIP

--- app/components/admin_dialog.py
+++ app/components/admin_dialog.py
@@ -16,26 +16,26 @@ def admin_dialog(state: StateManager):
     if state.get("show_admin_popup", False):
         @st.dialog("Admin Access")
         def _admin_dialog():
+            if not state.get("is_admin", False):
 
-            password = st.text_input(
-                "Password",
-                type="password"
-            )
+                password = st.text_input(
+                    "Password",
+                    type="password"
+                )
 
-            with st.container(horizontal=True, horizontal_alignment="right"):
-                if st.button("Cancel"):
-                    state.set("show_admin_popup", False)
-                    st.rerun()
-                if not state.get("is_admin", False):
+                with st.container(horizontal=True, horizontal_alignment="right"):
+                    if st.button("Cancel"):
+                        state.set("show_admin_popup", False)
+                        st.rerun()
                     if st.button("Login", type="primary"):
                         if password == st.secrets["ADMIN_PASSWORD"]:
                             state.set("is_admin", True)
                             state.set("show_admin_popup", False)
                             st.rerun()
-                else:
-                    if st.button("Logout", type="primary"):
-                        state.set("is_admin", False)
-                        state.set("show_admin_popup", False)
-                        st.rerun()
+            else:
+                if st.button("Logout", type="primary"):
+                    state.set("is_admin", False)
+                    state.set("show_admin_popup", False)
+                    st.rerun()
 
         _admin_dialog()
