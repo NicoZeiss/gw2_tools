20d4b50677b0c6b07fa89564a52aa7c6ffcb756d
E106727_michelin <nicolas.zeisser_ext@michelin.com>
2026-05-28 16:43:21 +0200

    WIP

--- app/components/admin_dialog.py
+++ app/components/admin_dialog.py
@@ -4,10 +4,10 @@ from streamlit_shortcuts import shortcut_button
 from state import StateManager
 
 
-def _action_btn(state: StateManager, password=None):
+def _action_btn(state: StateManager, valid_password=False):
     if not state.get("is_admin", False):
         if st.button("Login", type="primary"):
-            if password == st.secrets["ADMIN_PASSWORD"]:
+            if valid_password:
                 state.set("is_admin", True)
                 state.set("show_admin_popup", False)
                 st.rerun()
@@ -20,17 +20,19 @@ def _action_btn(state: StateManager, password=None):
 
 @st.dialog("Admin Access")
 def _admin_dialog(state: StateManager):
+    valid_password = False
     if not state.get("is_admin", False):
         password = st.text_input(
             "Password",
             type="password"
         )
+        valid_password = password == st.secrets["ADMIN_PASSWORD"]
 
     with st.container(horizontal=True, horizontal_alignment="right"):
         if st.button("Cancel"):
             state.set("show_admin_popup", False)
             st.rerun()
-        _action_btn(state, password)
+        _action_btn(state, valid_password)
 
 
 def admin_dialog(state: StateManager):
